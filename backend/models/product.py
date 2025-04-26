b1643c1141ac97b5c6aea41c47a5b12105a3a66d
rubenledesma10 <rubenledesma89@gmail.com>
2025-04-26 20:42:20 -0300

    change: modified name attribute

--- backend/models/product.py
+++ backend/models/product.py
@@ -7,15 +7,15 @@ class Product(db.Model):
     name=db.Column(db.String(50),nullnable=False)
     current_price=db.Column(db.Float, nullnable=False)
     stock=db.Column(db.Integer, nullnable=0)
-    id_supplier=db.Column(db.Integer,db.ForeignKey('suppliers.idSupplier'))
-    id_category=db.Column(db.Integer,db.ForeignKey('category.idCategory'))
+    supplier_id=db.Column(db.Integer,db.ForeignKey('suppliers.idSupplier'))
+    category_id=db.Column(db.Integer,db.ForeignKey('category.idCategory'))
 
-    def __init__(self, name, current_price, stock, id_supplier, id_category):
+    def __init__(self, name, current_price, stock, supplier_id, category_id):
         self.name=name
         self.current_price=current_price
         self.stock=stock
-        self.id_supplier=id_supplier
-        self.id_category=id_category
+        self.supplier_id=supplier_id
+        self.category_id=category_id
 
     def serialize(self):
         return{
@@ -23,6 +23,6 @@ class Product(db.Model):
             'name':self.name,
             'current_price':self.current_price,
             'stock':self.stock,
-            'id_supplier':self.id_supplier,
-            'id_category':self.id_category
+            'supplier_id':self.supplier_id,
+            'category_id':self.category_id
         }
