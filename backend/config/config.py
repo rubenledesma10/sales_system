3b6a9c4dfd35450e8745900b2af082f79bbf6314
rubenledesma10 <rubenledesma89@gmail.com>
2025-04-26 17:47:33 -0300

    first commit

---
+++ backend/config/config.py
@@ -0,0 +1,12 @@
+
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
+
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+database = os.getenv("MYSQL_DB")
+
+DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}/{database}"
