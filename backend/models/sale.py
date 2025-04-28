5509cade68a6f75af2c86eeac8a16bd5b4439f9b
SantiDev03 <romanosantiagonicolas@gmail.com>
2025-04-27 23:50:34 -0300

    Create sale entity

---
+++ backend/models/sale.py
@@ -0,0 +1,29 @@
+from models.db import db 
+
+class Sale(db.Model): 
+    __tablename__ ='sale'
+
+    id_sale = db.column(db.Integer, primary_key = True)
+    sale_date = db.column(db.Date, nullable = False)
+    discount = db.column(db.Float, nullable = False)
+    final_amount = db.column(db.Float, nullable = False)
+    id_client = db.column(db.Integer, db.ForeignKey('client.id_client'))
+    
+    client = db.relationship("Client", backref=db.backref("sales", lazy=True))
+    
+    def __init__(self,sale_date,discount,final_amount):
+        self.sale_date = sale_date
+        self.discount = discount
+        self.final_amount = final_amount 
+        
+    def __repr__(self): #Realizamos el repr, representacion, para mostrar el id, la  fecha y el id del cliente que realizo la venta.
+        return f"<Sale(id_sale={self.id_sale}, sale_date={self.sale_date}, client_id={self.id_client})>"
+
+    def serialize (self):
+        return {
+            'id_sale': self.id_sale,
+            'sale_date': self.sale_date.strftime(%d%m%Y), #Asi formateamos y detallamos la fecha con el anio,el horario y el mes.
+            'discount': self.discount,
+            'final_amount': self.final_amount,
+            'id_client': self.id_client
+        }
