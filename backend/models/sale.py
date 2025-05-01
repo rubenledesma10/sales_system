d9cc36e559f52a18560595371c65d4433f11550c
SantiDev03 <romanosantiagonicolas@gmail.com>
2025-05-01 18:06:23 -0300

    changes

--- backend/models/sale.py
+++ backend/models/sale.py
@@ -3,11 +3,11 @@ from models.db import db
 class Sale(db.Model): 
     __tablename__ ='sale'
 
-    id_sale = db.column(db.Integer, primary_key = True)
-    sale_date = db.column(db.Date, nullable = False)
-    discount = db.column(db.Float, nullable = False)
-    final_amount = db.column(db.Float, nullable = True)
-    id_client = db.column(db.Integer, db.ForeignKey('client.id_client'))
+    id_sale = db.Column(db.Integer, primary_key = True)
+    sale_date = db.Column(db.Date, nullable = False)
+    discount = db.Column(db.Float, nullable = False)
+    final_amount = db.Column(db.Float, nullable = True)
+    id_client = db.Column(db.Integer, db.ForeignKey('client.id_client'))
     
     client = db.relationship("Client", backref=db.backref("sales", lazy=True))
     sale_product =db.relationship('sale_product',backref='sale_product', lazy=True)
@@ -20,12 +20,12 @@ class Sale(db.Model):
     def __repr__(self): #Realizamos el repr, representacion, para mostrar el id, la  fecha y el id del cliente que realizo la venta.
         return f"<Sale(id_sale={self.id_sale}, sale_date={self.sale_date}, client_id={self.id_client})>"
 
-    def serialize (self):
-        return
-        {
+def serialize(self):
+    return {
             'id_sale': self.id_sale,
-            'sale_date': self.sale_date.strftime('%d%m%Y') #Asi formateamos y detallamos la fecha con el anio,el horario y el mes.
+            'sale_date': self.sale_date.strftime('%d%m%Y') if self.sale_date else None,
+            # asi formateamos y detallamos la fecha con el año,el horario y el mes.
             'discount': self.discount,
             'final_amount': self.final_amount,
             'id_client': self.id_client
-        }
+    }
