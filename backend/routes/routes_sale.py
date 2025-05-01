4781ccd15c972c9c5b3869085e5c38b0001b611f
SantiDev03 <romanosantiagonicolas@gmail.com>
2025-05-01 17:26:24 -0300

    Changes in sintaxis of branch sale

--- backend/routes/routes_sale.py
+++ backend/routes/routes_sale.py
@@ -26,41 +26,41 @@ def get_client_sales(client_id):
         return jsonify([sale.serialize() for sale in sales])
 
 @sale.route('/api/add_sale', methods = 'POST')
-def create_sale (): 
-        data = request.json()
-        
-        try:
-                id_client = data.get('id_client')
-                client = Client.query.get_or_404(id_client)
-                if not client:
-                        return jsonify ({'message': 'Client not found'}), 404
-        
-                sale_date_str = data.get ('sale_date')
-                sale_date = None
-                
-                if sale_date_str:
-                        try:
-                                sale_date = datetime.strptime(sale_date_str, '%d%m%Y').date()
-                        except ValueError:
-                                return jsonify({'message':'Incorrect date format. Must be ddmmyyyy'}), 400
-        sale = Sale(
-        sale_date=sale_date,
-        discount=data.get('discount', 0.0),
-        final_amount=data.get('final_amount'),
-        id_client= id_client
-        )
-        
-        db.session.add(sale)
-        db.session.flush() 
 
-        db.session.commit() 
+def create_sale():
+    data = request.get_json()
+
+    try:
+        id_client = data.get('id_client')
+        client = Client.query.get_or_404(id_client)
+        if not client:
+            return jsonify({'message': 'Client not found'}), 404
+
+        sale_date_str = data.get('sale_date')
+        sale_date = None
+
+        if sale_date_str:
+            try:
+                sale_date = datetime.strptime(sale_date_str, '%d%m%Y').date()
+            except ValueError:
+                return jsonify({'message': 'Incorrect date format. Must be ddmmyyyy'}), 400
+
+        sale = Sale(
+            sale_date=sale_date,
+            discount=data.get('discount', 0.0),
+            final_amount=data.get('final_amount'),
+            id_client=id_client
+        )
+
+        db.session.add(sale)
+        db.session.flush()
+        db.session.commit()
 
         return jsonify({'message': 'Sale created successfully', 'sale_id': sale.id_sale}), 201
 
-        except IntegrityError:
+    except IntegrityError:
         db.session.rollback()
         return jsonify({'message': 'Error de integridad en la base de datos'}), 400
-        
-        except Exception as e:
+    except Exception as e:
         db.session.rollback()
         return jsonify({'message': f'Error al crear la venta: {str(e)}'}), 500
