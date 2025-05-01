bc7eb0e8fcccf2d68d61bd8aec3c6742ee6d3d02
SantiDev03 <romanosantiagonicolas@gmail.com>
2025-05-01 20:49:10 -0300

    CHANGES FINALS SALE

--- backend/app.py
+++ backend/app.py
@@ -15,17 +15,15 @@ db.init_app(app)
 with app.app_context():
     from models.product import Product
     from models.sale_product import SaleProduct
-<<<<<<< HEAD
     from models.db import db
     from models.sale import Sale
     from datetime import datetime
-=======
     from models.category import Category
     from models.client import Client
     from models.phone import Phone
     from models.sale import Sale
     from models.supplier import Supplier
->>>>>>> f9f7c1ad3473ec4e32ae494a3332b0b320e8b3f7
+
     db.create_all()
 
 if __name__=='__main__':
