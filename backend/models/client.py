65237a55c5f6dec2a6dd30b2ecbf4b26bf6cf5e7
SantiDev03 <romanosantiagonicolas@gmail.com>
2025-05-01 20:35:52 -0300

    CAMBIOS

--- backend/models/client.py
+++ backend/models/client.py
@@ -1,22 +1,5 @@
 from models.db import db
-
-<<<<<<< HEAD
-class Client (db.Model):
-    __tablename__= "client"
-    
-    id_client= db.Column(db.Integer, primary_key= True)
-    name=db.Column(db.String(50), nullable = False)
-    rut = db.Column(db.String(50), nullable = False)
-    street_address=db.Column(db.String (50), nullable = False)
-    number_address=db.Column(db.String(50), nullable = False)
-    district_address=db.Column(db.String(50), nullable = False)
-    city_address = db.Column(db.String(50),nullable = False )
-    phone=db.relationship('phone', backref='phone', lazy=True)
-    sale=db.relationship('sale',backref='sale', lazy=True)
-    
-    def __init__(self,name,rut,street_address,number_address,district_address,city_address, phone, sale):
-        self.name = name
-=======
+        
 class Client(db.Model):
     __tablename__ = 'client'
 
@@ -32,7 +15,7 @@ class Client(db.Model):
     sales = db.relationship('Sale', backref='client', lazy=True)
 
     def __init__(self, rut, name, street_address, number_address, district_address, city_address):
->>>>>>> f9f7c1ad3473ec4e32ae494a3332b0b320e8b3f7
+
         self.rut = rut
         self.name = name
         self.street_address = street_address
