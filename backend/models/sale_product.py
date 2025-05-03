1e8258153c51c9354cc42517a67ac36a1568a803
rubenledesma10 <rubenledesma89@gmail.com>
2025-05-03 14:48:21 -0300

    finished routes sale-product and fix sale_product.json and fix entity sale_product

--- backend/models/sale_product.py
+++ backend/models/sale_product.py
@@ -1,4 +1,6 @@
 from models.db import db
+from models.sale import Sale
+from models.product import Product
 
 class SaleProduct(db.Model):
     __tablename__ = 'sale_product'
@@ -6,23 +8,24 @@ class SaleProduct(db.Model):
     id_sale_product = db.Column(db.Integer, primary_key=True)
     subtotal = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    sold_price = db.Column(db.Float, nullable=False)
-    id_product = db.Column(db.Integer, db.ForeignKey('product.id_product'), nullable=False)
-    id_sale = db.Column(db.Integer, db.ForeignKey('sale.id_sale'), nullable=False)
+    sould_price = db.Column(db.Float, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id_product'), nullable=False)
+    sale_id = db.Column(db.Integer, db.ForeignKey('sale.id_sale'), nullable=False)
 
-    def __init__(self, subtotal, quantity, sold_price, id_product, id_sale):
+
+    def __init__(self, subtotal, quantity, sould_price, product_id, sale_id):
         self.subtotal = subtotal
         self.quantity = quantity
-        self.sold_price = sold_price
-        self.id_product = id_product
-        self.id_sale = id_sale
+        self.sould_price = sould_price
+        self.product_id = product_id
+        self.sale_id = sale_id
 
     def serialize(self):
         return {
             'id_sale_product': self.id_sale_product,
             'subtotal': self.subtotal,
             'quantity': self.quantity,
-            'sold_price': self.sold_price,
-            'id_product': self.id_product,
-            'id_sale': self.id_sale
+            'sould_price': self.sould_price,
+            'product_id': self.product_id,
+            'sale_id': self.sale_id
         }
